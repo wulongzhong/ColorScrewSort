7283fad38a79052cee4031863fea68e1663ebb1a
wulongzhong <1611136987@qq.com>
2024-01-24 14:40:54 +0800

    Init

---
+++ Assets/GamePlay/Scripts/Game/LevelPlayMgr.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPlayMgr : MonoBehaviour
+{
+    public void LoadLevel(ConfigPB.LevelData levelData, Action onFinish)
+    {
+
+    }
+}
---
+++ Assets/GamePlay/Scripts/Game/NutBev.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NutBev : MonoBehaviour
+{
+    public int color;
+    public bool moving;
+    public int currPosY;
+    public bool bMasking;
+
+    public void Init(int color)
+    {
+        this.color = color;
+
+        switch(color)
+        {
+            case 0:
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+    public void MoveToNewStick(StickBev stickBev)
+    {
+
+    }
+
+    public void MoveToUp()
+    {
+
+    }
+
+    public void MoveToDown()
+    {
+
+    }
+
+    public void OpenMask()
+    {
+
+    }
+}
